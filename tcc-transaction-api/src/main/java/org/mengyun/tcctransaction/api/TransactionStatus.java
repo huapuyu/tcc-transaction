e5ce5be53d3d6d300ac89c42bbc2de983c9375f8
朱振 <anders.zhu@vipshop.com>
2016-05-13 01:22:26 +0800

    format code

--- tcc-transaction-api/src/main/java/org/mengyun/tcctransaction/api/TransactionStatus.java
+++ tcc-transaction-api/src/main/java/org/mengyun/tcctransaction/api/TransactionStatus.java
@@ -1,33 +1,27 @@
 package org.mengyun.tcctransaction.api;
 
-
-/**
- * Created by changmingxie on 10/28/15.
- */
 public enum TransactionStatus {
 
-    TRYING(1), CONFIRMING(2), CANCELLING(3);
+	TRYING(1), CONFIRMING(2), CANCELLING(3);
 
-    private int id;
+	private int id;
 
-     TransactionStatus(int id) {
-        this.id = id;
-    }
+	TransactionStatus(int id) {
+		this.id = id;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public static TransactionStatus valueOf(int id) {
-
-        switch (id) {
-            case 1:
-                return TRYING;
-            case 2:
-                return CONFIRMING;
-            default:
-                return CANCELLING;
-        }
-    }
-
+	public static TransactionStatus valueOf(int id) {
+		switch (id) {
+		case 1:
+			return TRYING;
+		case 2:
+			return CONFIRMING;
+		default:
+			return CANCELLING;
+		}
+	}
 }
--- tcc-transaction-api/src/main/java/org/mengyun/tcctransaction/api/TransactionXid.java
+++ tcc-transaction-api/src/main/java/org/mengyun/tcctransaction/api/TransactionXid.java
@@ -1,98 +1,88 @@
 package org.mengyun.tcctransaction.api;
 
-
 import javax.transaction.xa.Xid;
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.UUID;
 
-/**
- * Created by changmingxie on 10/26/15.
- */
 public class TransactionXid implements Xid, Serializable {
 
-    private static final long serialVersionUID = -6817267250789142043L;
-    private int formatId = 1;
+	private static final long serialVersionUID = -6817267250789142043L;
+	private int formatId = 1;
+	private byte[] globalTransactionId;
+	private byte[] branchQualifier;
 
-    private byte[] globalTransactionId;
+	public TransactionXid() {
+		globalTransactionId = UUID.randomUUID().toString().getBytes();
+		branchQualifier = UUID.randomUUID().toString().getBytes();
+	}
 
-    private byte[] branchQualifier;
+	public TransactionXid(byte[] globalTransactionId) {
+		this.globalTransactionId = globalTransactionId;
+		branchQualifier = UUID.randomUUID().toString().getBytes();
+	}
 
-    public TransactionXid() {
-        globalTransactionId = UUID.randomUUID().toString().getBytes();
-        branchQualifier = UUID.randomUUID().toString().getBytes();
-    }
+	public TransactionXid(byte[] globalTransactionId, byte[] branchQualifier) {
+		this.globalTransactionId = globalTransactionId;
+		this.branchQualifier = branchQualifier;
+	}
 
-    public TransactionXid(byte[] globalTransactionId) {
-        this.globalTransactionId = globalTransactionId;
-        branchQualifier = UUID.randomUUID().toString().getBytes();
-    }
+	@Override
+	public int getFormatId() {
+		return formatId;
+	}
 
-    public TransactionXid(byte[] globalTransactionId, byte[] branchQualifier) {
-        this.globalTransactionId = globalTransactionId;
-        this.branchQualifier = branchQualifier;
-    }
+	@Override
+	public byte[] getGlobalTransactionId() {
+		return globalTransactionId;
+	}
 
-    @Override
-    public int getFormatId() {
-        return formatId;
-    }
+	@Override
+	public byte[] getBranchQualifier() {
+		return branchQualifier;
+	}
 
-    @Override
-    public byte[] getGlobalTransactionId() {
-        return globalTransactionId;
-    }
+	@Override
+	public String toString() {
+		return UUID.nameUUIDFromBytes(globalTransactionId).toString() + "|" + UUID.nameUUIDFromBytes(branchQualifier).toString();
+	}
 
-    @Override
-    public byte[] getBranchQualifier() {
-        return branchQualifier;
-    }
+	public TransactionXid clone() {
+		byte[] cloneGlobalTransactionId = new byte[globalTransactionId.length];
+		byte[] cloneBranchQualifier = new byte[branchQualifier.length];
 
-    @Override
-    public String toString() {
-        return UUID.nameUUIDFromBytes(globalTransactionId).toString() + "|" + UUID.nameUUIDFromBytes(branchQualifier).toString();
-    }
+		System.arraycopy(globalTransactionId, 0, cloneGlobalTransactionId, 0, globalTransactionId.length);
+		System.arraycopy(branchQualifier, 0, cloneBranchQualifier, 0, branchQualifier.length);
 
-    public TransactionXid clone() {
+		TransactionXid clone = new TransactionXid(cloneGlobalTransactionId, cloneBranchQualifier);
+		return clone;
+	}
 
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + this.getFormatId();
+		result = prime * result + Arrays.hashCode(branchQualifier);
+		result = prime * result + Arrays.hashCode(globalTransactionId);
+		return result;
+	}
 
-        byte[] cloneGlobalTransactionId = new byte[globalTransactionId.length];
-        byte[] cloneBranchQualifier = new byte[branchQualifier.length];
-
-        System.arraycopy(globalTransactionId, 0, cloneGlobalTransactionId, 0, globalTransactionId.length);
-        System.arraycopy(branchQualifier, 0, cloneBranchQualifier, 0, branchQualifier.length);
-
-        TransactionXid clone = new TransactionXid(cloneGlobalTransactionId, cloneBranchQualifier);
-        return clone;
-    }
-
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + this.getFormatId();
-        result = prime * result + Arrays.hashCode(branchQualifier);
-        result = prime * result + Arrays.hashCode(globalTransactionId);
-        return result;
-    }
-
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        } else if (obj == null) {
-            return false;
-        } else if (getClass() != obj.getClass()) {
-            return false;
-        }
-        TransactionXid other = (TransactionXid) obj;
-        if (this.getFormatId() != other.getFormatId()) {
-            return false;
-        } else if (Arrays.equals(branchQualifier, other.branchQualifier) == false) {
-            return false;
-        } else if (Arrays.equals(globalTransactionId, other.globalTransactionId) == false) {
-            return false;
-        }
-        return true;
-    }
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		} else if (obj == null) {
+			return false;
+		} else if (getClass() != obj.getClass()) {
+			return false;
+		}
+		TransactionXid other = (TransactionXid) obj;
+		if (this.getFormatId() != other.getFormatId()) {
+			return false;
+		} else if (Arrays.equals(branchQualifier, other.branchQualifier) == false) {
+			return false;
+		} else if (Arrays.equals(globalTransactionId, other.globalTransactionId) == false) {
+			return false;
+		}
+		return true;
+	}
 }
-
-
